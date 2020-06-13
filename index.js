a35b127d443374d04eddbb4a90b1a1f5f5d5638e
Willem Verbuyst <willemverbuyst@gmail.com>
2020-06-13 07:39:23 +0200

    Add route to get all customers

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const orderRouter = require('./routers/order');
 const productRouter = require('./routers/products');
 const authRouter = require('./routers/auth');
+const customerRouter = require('./routers/customers');
+
 const cors = require('cors');
 
 const PORT = process.env.PORT || 4000;
@@ -16,5 +18,6 @@ app.use(jsonParser);
 app.use('/', authRouter);
 app.use('/orders', orderRouter);
 app.use('/products', productRouter);
+app.use('/customers', customerRouter);
 
 app.listen(PORT, console.log(`listening op port: ${PORT}`));
---
+++ routers/customers.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const { Router } = express;
+const router = new Router();
+
+const Customers = require('../models').customer;
+
+//get all products
+router.get('/', async (req, res, next) => {
+  try {
+    const limit = Math.min(req.query.limit || 25, 500);
+    const offset = req.query.offset || 0;
+    const customers = await Customers.findAndCountAll({
+      limit,
+      offset,
+    }).then((result) =>
+      res.send({ customers: result.rows, total: result.count })
+    );
+  } catch (e) {
+    next(e);
+  }
+});
